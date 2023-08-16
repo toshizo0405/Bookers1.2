c8bdfba9323da789c735f13b8ae46b770be1f3db
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-08-16 14:04:30 +0000

    [Update15]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,7 +33,7 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
-     else
+    else
       render :edit
     end
   end
